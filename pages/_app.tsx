2c2ef8c515d98d27f9d69bf3088ed12b12b9d884
Bruno Torquato <brunohenriquecosta2014@gmail.com>
2022-12-16 18:24:10 -0300

    Commiting full project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Cabecalho from '../src/components/Cabecalho/Cabecalho'
+import { ThemeProvider } from '@mui/material'
+import tema from '../src/themes/themes'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={tema}>
+      <Cabecalho></Cabecalho>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+export default MyApp
